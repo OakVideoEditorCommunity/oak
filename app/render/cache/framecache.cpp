08d27e75c0840c7680e441cfc12caeb5aa9bb99b
Mike Solar <iam@mikesolar.cn>
2026-01-24 21:11:27 +0800

    feat: Implement LRU frame cache with memory-based eviction
    - Add platform-specific memory detection (Windows/Linux/macOS)
    - Implement LRU eviction thread that removes oldest entries when cache exceeds 25% of available memory
    - Add thread-safe operations with mutex protection
    - Extend FrameCacheKey with equality operator
    - Add FrameCacheEntry size calculation including overhead
    - Add proper cleanup in destructor with finalize() method

--- app/render/cache/framecache.cpp
+++ app/render/cache/framecache.cpp
@@ -1,2 +1,111 @@
 #include "framecache.h"
-olive::cache::FrameCache olive::cache::FrameCache::frame_cache_;
+#include <mutex>
+#include <qsysinfo.h>
+#include <xcb/xproto.h>
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
+#include <QSysInfo>
+
+#ifdef _WIN32
+#include <windows.h>
+#elif defined(__linux__)
+#include <stdio.h>
+#include <string.h>
+#else // macOS
+#include <sys/sysctl.h>
+#include <mach/mach.h>
+#include <mach/vm_statistics.h>
+#endif
+
+uint64_t get_available_memory()
+{
+#ifdef _WIN32
+	MEMORYSTATUSEX status;
+	status.dwLength = sizeof(status);
+	if (GlobalMemoryStatusEx(&status)) {
+		return status.ullAvailPhys; 
+	}
+	return 0;
+
+#elif defined(__linux__)
+	FILE *fp = fopen("/proc/meminfo", "r");
+	if (!fp) {
+		return 0;
+	}
+
+	char line[256];
+	uint64_t available_kb = 0;
+
+	while (fgets(line, sizeof(line), fp)) {
+		if (sscanf(line, "MemAvailable: %lu kB", &available_kb) == 1) {
+			fclose(fp);
+			return available_kb * 1024; 
+		}
+	}
+
+	fclose(fp);
+	return 0;
+
+#else
+	vm_size_t page_size;
+	vm_statistics64_data_t vm_stats;
+	mach_port_t mach_port = mach_host_self();
+	mach_msg_type_number_t count = sizeof(vm_stats) / sizeof(natural_t);
+
+    if (KERN_SUCCESS != host_page_size(mach_port, &page_size)) {
+		return 0;
+	}
+
+	if (KERN_SUCCESS != host_statistics64(mach_port, HOST_VM_INFO64,
+										  (host_info64_t)&vm_stats, &count)) {
+		return 0;
+	}
+
+	return (uint64_t)vm_stats.free_count * page_size;
+#endif
+}
+
+void my_sleep(unsigned int seconds)
+{
+#ifdef _WIN32
+	Sleep(seconds * 1000);
+#else
+	sleep(seconds);
+#endif
+}
+
+using namespace olive::cache;
+FrameCache FrameCache::frame_cache_;
+
+inline void my_sleep(){
+
+}
+
+void FrameCache::thread(){
+    while(!stop){
+        std::unique_lock<std::mutex> lock(size_lock);
+        lock.lock();
+		cache_size = get_available_memory() * 0.25;
+
+        size_t total_cache_size = 0;
+
+        for(auto entry: map_){
+            total_cache_size += entry->size();
+        }
+
+        if(total_cache_size > cache_size){
+            while(total_cache_size > cache_size){
+                FrameCacheKey key = lru_list.last();
+                lru_list.removeLast();
+                size_t size=map_[key]->size();
+                map_.remove(key);
+                total_cache_size -= size;
+            }
+        }
+        lock.unlock();
+		my_sleep(5);
+    }
+}
--- app/render/cache/framecache.h
+++ app/render/cache/framecache.h
@@ -1,12 +1,19 @@
 
+#include "gtest/gtest.h"
 #include "olive/core/util/rational.h"
 #include "render/texture.h"
 #include "render/videoparams.h"
 #include <cstddef>
 #include <cstdint>
 #include <ctime>
+#include <functional>
 #include <memory>
-#include <qhash.h>
+#include <mutex>
+#include <QHash>
+#include <QThread>
+#include <qobject.h>
+#include <qtmetamacros.h>
+#include <thread>
 #include <utility>
 namespace olive::cache{
 class FrameCacheKey{
@@ -27,6 +34,10 @@ public:
     VideoParams params(){
         return params_;
     }
+    bool operator==(FrameCacheKey &other){
+        return version_==other.version_ && time_ == other.time_
+            && params_ == other.params_;
+    }
 };
 class FrameCacheEntry{
 private:
@@ -40,29 +51,89 @@ public:
         texture_ = texture;
 		last_use = std::time(nullptr);
 	}
+    TexturePtr texture(){
+        last_use = std::time(nullptr);
+        return texture_;
+    }
+    time_t last_use_time(){
+        return last_use;
+    }
+
+    size_t size(){
+        int byte_per_channel = texture_->format().byte_count();
+        int width = texture_->width();
+        int height = texture_->height();
+        int channel_count = texture_->channel_count();
+		int overhead_factor = 1.3; // std::shared_ptr, metadata
+        return byte_per_channel * width * height * channel_count * overhead_factor;
+	}
 
 };
-
+using FrameCacheEntryPtr = std::shared_ptr<FrameCacheEntry>;
 /*
  * LRU Frame Cache
  * TODO: This is just a mininum version. It should 
  * be replaced by a more complex one.
+ * We use 25% of availiable memory as LRU Cache.
  */
-class FrameCache{
-    QHash<FrameCacheKey, FrameCacheEntry> map_;
+	class FrameCache {
+private:
+    QHash<FrameCacheKey, FrameCacheEntryPtr> map_;
     static FrameCache frame_cache_;
+    std::mutex map_lock;
+    std::mutex size_lock;
+    size_t cache_size;
+    bool stop = false;
+    std::thread *gc_thread;
+    QList<FrameCacheKey> lru_list;
 public:
     static FrameCache* getInstance(){
         return &frame_cache_;
     }
-    FrameCache() = default;
-    ~FrameCache() = default;
+    FrameCache(){
+        auto f = std::bind(&FrameCache::thread, this);
+        gc_thread=new std::thread(f);
+    };
+    ~FrameCache(){
+        finalize();
+    };
 
-    FrameCacheEntry &get(FrameCacheKey& key){
-        return map_[key];
+    FrameCacheEntryPtr get(FrameCacheKey& key){
+
+        if(map_.contains(key)){
+			if (lru_list.last() != key) {
+				lru_list.removeOne(key);
+				lru_list.append(key);
+			}
+			return map_[key];
+		}
+        else{
+            return nullptr;
+        }
+        
     }
-    void set(FrameCacheKey &key, FrameCacheEntry &entry){
+    void set(FrameCacheKey &key, FrameCacheEntryPtr entry){
+        std::lock_guard<std::mutex> lock(this->map_lock);
         map_[key]=entry;
+        if(!lru_list.contains(key)){
+            lru_list.append(key);
+        }
+        else {
+            lru_list.removeOne(key);
+			lru_list.append(key);
+		}
     }
+    void remove(FrameCacheKey &key){
+		std::lock_guard<std::mutex> lock(this->map_lock);
+        lru_list.removeOne(key);
+		map_.remove(key);
+    }
+
+    void finalize(){
+        stop = true;
+        gc_thread->join();
+    }
+protected:
+    void thread();
 };
 }
